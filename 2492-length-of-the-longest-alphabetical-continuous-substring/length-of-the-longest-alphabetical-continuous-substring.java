cb3332fcc983c2dd1ea81519a6a9c950d0735a5a
poojitha1743 <saiveerapoojithapalla@gmail.com>
2026-08-11 15:43:16 +0530

    Time: 12 ms (75.07%) | Memory: 47.6 MB (43.16%) - LeetSync

---
+++ 2492-length-of-the-longest-alphabetical-continuous-substring/length-of-the-longest-alphabetical-continuous-substring.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestContinuousSubstring(String s) {
+      int maxLen =1;
+      int count =1;
+      for(int i=1;i<s.length();i++)
+      {
+        if(s.charAt(i)-s.charAt(i-1)==1)
+        {
+           count++;
+        }
+        else
+        {
+            count=1;
+        }
+        maxLen = Math.max(maxLen,count);
+      }
+      return maxLen;
+    }
+}
