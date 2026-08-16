e6180f7646603c9fe88589f066c5ecc3b949fd24
poojitha1743 <saiveerapoojithapalla@gmail.com>
2026-08-16 22:07:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (80.80%) - LeetSync

---
+++ 1542-consecutive-characters/consecutive-characters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxPower(string s) {
+       int count = 1;
+        int maxCount = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s[i] == s[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            maxCount = max(maxCount, count);
+        }
+
+        return maxCount; 
+    }
+};
